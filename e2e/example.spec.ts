0feb9edd10ed321ac3fea69a1582c1ee58b087a7
Stefan Trauth <mail@stefantrauth.de>
2025-12-02 13:52:10 +0100

    chore: update smoke test to navigate to the root URL

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,6 +1,5 @@
 import test, { expect } from '@playwright/test';
 
 test('Smoke test', async ({ page }) => {
-  await page.goto('https://www.google.com');
-  await expect(page).toHaveTitle('Google');
+  await page.goto('/');
 });
