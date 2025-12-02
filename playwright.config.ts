dcf3a2eec948fa9174d1426d249fd4bb94bff740
Stefan Trauth <mail@stefantrauth.de>
2025-12-02 15:38:18 +0100

    chore: uncomment trace option in Playwright config for better debugging

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    // trace: 'on',
 
     /* can also be defined per project below in the use block */
     timezoneId: 'Europe/Berlin',
