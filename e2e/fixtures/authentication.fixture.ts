dad01ab2f4c1d7437c71e29860787ac698181f99
Stefan Trauth <mail@stefantrauth.de>
2025-12-03 13:27:30 +0100

    chore: enhance authentication fixture with user creation and counting functionalities

--- e2e/fixtures/authentication.fixture.ts
+++ e2e/fixtures/authentication.fixture.ts
@@ -2,6 +2,12 @@ import { test as base, expect } from '@playwright/test';
 
 interface AuthenticationFixture {
   loginUser: (email: string, password: string) => Promise<void>;
+  createUser: (user: {
+    name: string;
+    email: string;
+    role: string;
+  }) => Promise<void>;
+  countUsers: () => Promise<number>;
 }
 
 export const test = base.extend<AuthenticationFixture>({
@@ -13,6 +19,23 @@ export const test = base.extend<AuthenticationFixture>({
       await page.getByRole('button', { name: 'Submit sign in form' }).click();
 
       await expect(page).toHaveURL('/');
+    });
+  },
+  createUser: async ({ page }, use) => {
+    await use(async (user: { name: string; email: string; role: string }) => {
+      await page.goto('/fixtures-demo');
+      await page.getByLabel('Name').fill(user.name);
+      await page.getByLabel('Email').fill(user.email);
+      await page.getByLabel('Role').selectOption({ value: user.role });
+      await page.getByRole('button', { name: 'Add User' }).click();
+    });
+  },
+  countUsers: async ({ page }, use) => {
+    await use(async () => {
+      let userCountString =
+        (await page.getByTestId('user-count').textContent()) || '';
+      userCountString = userCountString?.replace(' users', '');
+      return parseInt(userCountString);
     });
   },
 });
