9b320531ee72058ac92277510580f1d1da06396c
Stefan Trauth <mail@stefantrauth.de>
2025-12-03 09:59:05 +0100

    chore: add authentication setup and environment variables for user credentials

---
+++ e2e/setup/auth.setup.ts
@@ -0,0 +1,28 @@
+import { test as setup, expect } from '@playwright/test';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const userFile = path.join(__dirname, '../../playwright/.auth/user.json');
+
+setup.beforeEach(async ({ page }) => {
+  await page.goto('/auth/signin');
+});
+
+setup('should sign in with valid credentials', async ({ page }) => {
+  const username = process.env.TEST_USERNAME;
+  const userPassword = process.env.TEST_PASSWORD;
+
+  setup.skip(
+    !username || !userPassword,
+    'Missing TEST_USERNAME or TEST_PASSWORD env variables',
+  );
+
+  const email = page.getByLabel('Email');
+  const password = page.getByLabel('Password');
+  await email.fill(username!);
+  await password.fill(userPassword!);
+  await page.getByRole('button', { name: 'Submit sign in form' }).click();
+
+  await page.context().storageState({ path: userFile });
+});
